9726021122aa709f409ccdb76e2773b019c2fca2
t-doke <163096913+t-doke@users.noreply.github.com>
2026-08-17 12:25:11 +0900

    sqlファイルを変更

---
+++ data/table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE accounts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    picture LONGBLOB NULL
+);
+CREATE TABLE lists (
+    id INT AUTO_INCREMENT PRIMARY KEY,    -- 主キー
+    number VARCHAR(255) NOT NULL,         -- 社員番号
+    name VARCHAR(255) NOT NULL,           -- 名前
+    password VARCHAR(255) NOT NULL,       -- パスワード
+    paid INT NOT NULL,                    -- 有給数
+    company_id VARCHAR(255) NOT NULL,       -- 企業ID
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  -- 作成日時
+    updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  -- 更新日時
+);
